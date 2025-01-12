761200d920747556fd0dbdd9bbb84bbc6e0a1c85
Rafik <rafik7704@gmail.com>
2025-01-12 13:58:14 +0800

    example gettter and setter

---
+++ src/main/java/com/miniprog/demo/model/Token.java
@@ -0,0 +1,23 @@
+package com.miniprog.demo.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@Document(collection = "Token", language = "none")
+public class Token {
+    @Id
+    private String id;
+
+    @Indexed(unique = true)
+    private String token;
+
+}
